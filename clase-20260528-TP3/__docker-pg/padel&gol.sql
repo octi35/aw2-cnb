95634a7bc6b62b84102f4b505a3382ad4e2e316d
octi35 <octifaki@gmail.com>
2026-05-28 19:28:03 -0300

    Actualizar archivos SQL y Docker para padel&gol

--- clase-20260528-TP3/__docker-pg/padel&gol.sql
+++ clase-20260528-TP3/__docker-pg/padel&gol.sql
@@ -1,23 +1,23 @@
 -- Conectar a la base de datos 'padel&gol'
-\c tienda;
+\c "padel&gol";
 
 -- Crear la tabla 'canchas'
 CREATE TABLE canchas (
     id SERIAL PRIMARY KEY,
-    nombre VARCHAR(100),
+    nombre VARCHAR(100) NOT NULL,
     deporte VARCHAR(100),
     precio NUMERIC(10, 2),
-    capacidad NUMERIC(10, 2),
-    disponible BOOLEAN
+    capacidad INT,
+    disponible BOOLEAN DEFAULT true
 );
 
 CREATE TABLE reservas (
     id SERIAL PRIMARY KEY,
-    cancha VARCHAR(100),
-    usuario VARCHAR(100),
+    cancha INT REFERENCES canchas(id) ON DELETE CASCADE,
+    usuario VARCHAR(100) NOT NULL,
     fecha VARCHAR(20),
     hora VARCHAR(10),
-    estado VARCHAR(20)
+    estado VARCHAR(20) DEFAULT 'Confirmada'
 );
 
 -- Insertar 10 canchas
@@ -31,12 +31,13 @@ INSERT INTO canchas (nombre, deporte, precio, capacidad, disponible) VALUES
     ('La Pared de Cristal', 'Pádel', 11000, 4, false),
     ('Tie Break Arena', 'Pádel', 10000, 4, false),
     ('Volea Master', 'Pádel', 8500, 4, true),
+    ('Estadio Central', 'Tenis', 12000, 2, true);
 
 -- Insertar reservas
 INSERT INTO reservas (cancha, usuario, fecha, hora, estado) VALUES
-    ('La Bombonera del Barrio', 'Carlos Rodríguez', '2024-05-10', '19:00', 'Confirmada'),
-    ('Camp Nou de la Esquina', 'Federico Gómez', '2024-05-10', '21:00', 'Confirmada'),
-    ('Estadio del Sol', 'Martín Sosa', '2024-05-11', '18:00', 'Confirmada'),
-    ('Predio El 10', 'Julián Álvarez', '2024-05-11', '20:00', 'Confirmada'),
-    ('Padel Point', 'Lucía Fernández', '2024-05-10', '17:30', 'Confirmada'),
-    ('Tie Break Arena', 'Esteban Quito', '2024-05-12', '10:00', 'Confirmada'),
+    (1, 'Carlos Rodríguez', '2024-05-10', '19:00', 'Confirmada'),
+    (2, 'Federico Gómez', '2024-05-10', '21:00', 'Confirmada'),
+    (3, 'Martín Sosa', '2024-05-11', '18:00', 'Confirmada'),
+    (4, 'Julián Álvarez', '2024-05-11', '20:00', 'Confirmada'),
+    (6, 'Lucía Fernández', '2024-05-10', '17:30', 'Confirmada'),
+    (8, 'Esteban Quito', '2024-05-12', '10:00', 'Confirmada');
